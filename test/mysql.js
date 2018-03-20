f13a71b3863bd9bdee0b5c8cde9eaaeedc13874c
chenhao <302835677@qq.com>
2018-03-21 00:29:52 +0800

    feat: mysql事务操作

--- test/mysql.js
+++ test/mysql.js
@@ -3,13 +3,25 @@
 const sql = require('../index');
 
 const mysqlPool = sql.mysql.init({
-    database: 'waihui',
+    database: 'test',
     user: 'root',
     password: '123456'
 });
+const _getNewSqlParamEntity = sql.mysql._getNewSqlParamEntity;
 
 async function test () {
-    const data = await sql.mysql.dataCenter(mysqlPool, 'select * from t_category');
+    const sqlParamsEntity = [];
+    const sql1 = 'insert ?? set name = ?, age = ?, sex = ?';
+    const param1 = ['tbl_user', 'test1', 20, 1];
+    sqlParamsEntity.push(_getNewSqlParamEntity(sql1, param1));
+
+    const sql2 = 'insert ?? set name = ?, age = ?, sex = ?';
+    const param2 = ['tbl_user', 'test2', 22, 0];
+    sqlParamsEntity.push(_getNewSqlParamEntity(sql2, param2));
+    const data = await sql.mysql.execTrans(mysqlPool, sqlParamsEntity);
+
+    // const sql1 = 'select * from ??';
+    // const data = await sql.mysql.exec(mysqlPool, sql1, ['tbl_user']);
     console.log(data);
 }
 
